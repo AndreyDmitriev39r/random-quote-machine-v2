1e14246fbfc4562767c87c73726799fbc1c4771d
Andrey Dmitriev <admit8490@gmail.com>
2023-11-10 18:49:15 +0200

    refactored component tree

--- src/components/NewQuote.tsx
+++ src/components/NewQuote.tsx
@@ -1,6 +1,6 @@
 const NewQuote = () => {
   return (
-    <button className="bg-cyan-400 rounded-lg font-semibold border-1 shadow-md shadow-stone-400">New quote, please!</button>
+    <button>New quote, please!</button>
   )
 }
 
