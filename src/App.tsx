e711dc9fd2cc3557fd06d55c789e7390e211e3fd
Andrey Dmitriev <admit8490@gmail.com>
2023-11-21 23:01:23 +0200

    functionality for 2 new quote types

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ const App = () => {
   const initialQuote = {
     text: "text for quote on initial render",
     author: "author of initial quote"
-  };
+  };  
 
   const options = [
     "random",
     "inspiring",
-    "type1",
-    "type2",
+    "movies",
+    "historical",
   ] as const;  
 
   type QuoteTypesUnion = (typeof options)[number];
@@ -43,15 +43,33 @@ const App = () => {
     }
   }
 
+  const getMoviesQuote = async () => {
+    try {
+     const response = await axios.get("https://api.api-ninjas.com/v1/quotes?category=movies", {headers: { 'X-Api-Key': import.meta.env.VITE_API_KEY}},);     
+     setQuote({text: response.data[0].quote, author: response.data[0].author});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getHistoricalQuote = async () => {
+    try {
+     const response = await axios.get("https://api.api-ninjas.com/v1/quotes?category=history", {headers: { 'X-Api-Key': import.meta.env.VITE_API_KEY}},);     
+     setQuote({text: response.data[0].quote, author: response.data[0].author});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   const fetchQuote = (quoteType: QuoteTypesUnion) => {
     if (quoteType === "random") {
       setQuote({text: "random quote text", author: "random quote author"})
     } else if (quoteType === "inspiring") {
       getInsipiringQuote();      
-    } else if (quoteType === "type1") {
-      setQuote({text: "type1 quote text", author: "type1 quote author"})
-    } else if (quoteType === "type2") {
-      setQuote({text: "type2 quote text", author: "type2 quote author"})
+    } else if (quoteType === "movies") {
+      getMoviesQuote();
+    } else if (quoteType === "historical") {
+      getHistoricalQuote();
     }
   }
 
